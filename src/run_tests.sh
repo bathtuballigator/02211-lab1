1dc183cf78afe57d77ba84a11b35ffa32541b820
ninawrona <ninawrona@gmail.com>
2026-03-01 13:08:47 +0100

    Corrected readme, and test script + cleanup

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -3,30 +3,15 @@
 # part 1:
 for FILE in test_*.act; do 
   echo $FILE; 
-  aflat -Tsky130l $FILE > $FILE.prs
+  aflat -Tsky130l $FILE > prs/$FILE.prs
 done
 
 # part 2, buffer:
 echo "cycle
 exit
-" | actsim -Wlang_subst:off testbuf.act test > pt2_buftest.log
+" | actsim -Wlang_subst:off testbuf.act test > log/pt2_buftest.log 2>&1
 
-# pseudocode:
-#   source:
-#     if recieving data + push, send data to buf[0]. when buf[0] is full and can't push next element down, send will stall
-#   sink:
-#     if getting pop signal, remove data from buf[0] and enable empty indicator
-#
-#   buffer element:
-#   loop:
-#     attempt send data up (if first buffer, will try and send to sink)
-#     OR
-#     wait for data on input
-#
-#
-#     if input > current value,
-#       send input to next buffer (will wait for ack)
-#     else,
-#       send current value to next buffer (will wait for ack)
-#       set input to current value
-
+# part 2, priority queue:
+echo "cycle
+exit
+" | actsim -Wlang_subst:off testprio.act test > log/pt2_priotest.log 2>&1
